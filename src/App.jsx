1bc589922867ee5ae96ba7a2b4536f6c2dc8a701
Alex <redj9464@gmail.com>
2025-01-22 20:00:42 +0200

    Add contactList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css'
+import { useState } from 'react';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
 
-function App() {
-  return 
-}
+const App = () => {
+  const [contacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-export default App
+  const handleFilterChange = (value) => {
+    setFilter(value);
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <SearchBox filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} />
+    </div>
+  );
+};
+
+export default App;
